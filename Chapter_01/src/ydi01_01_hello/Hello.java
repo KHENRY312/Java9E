8f0fdd8bb455f216d0d488bde3846a5c49ded7eb
Kim <kimberly.henry@outlook.com>
2022-12-03 19:04:19 -0500

    J9E-50 YDI01_01-Hello

---
+++ Chapter_01/src/ydi01_01_hello/Hello.java
@@ -0,0 +1,16 @@
+package ydi01_01_hello;
+/*
+Project:    Java9E
+Module:     Chapter_01
+Package:    ydi01_01_hello
+File:       Hello
+Created By: Kimberly Henry
+Created On: Saturday, 03-Dec-2022
+*/
+public class Hello
+	{
+       public static void main(String[] args)
+			{
+				System.out.println("Hello, world!");
+			}
+	}
