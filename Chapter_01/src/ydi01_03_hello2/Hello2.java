beb536a916fd7c0df643a19ad158f2c1f4b9e32b
Kim <kimberly.henry@outlook.com>
2022-12-03 19:37:37 -0500

    J9E-52 YDI01_03-Hello2

---
+++ Chapter_01/src/ydi01_03_hello2/Hello2.java
@@ -0,0 +1,19 @@
+package ydi01_03_hello2;
+
+/*
+Project:       Java9E
+Module:      Chapter_01
+Package:     ydi01_03_hello2
+File:            Hello2
+Created By: Kimberly Henry
+Created On: Saturday, 03-Dec-2022
+*/public class Hello2
+	{
+	public static void main(String[] args)
+		{
+		  System.out.println("Hello, world!");
+        
+        /* This class demonstrates the use of the println()
+          method to print the message Hello, world! */
+		}
+	}
