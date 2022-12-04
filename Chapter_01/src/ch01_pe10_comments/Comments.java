2f1812532f22d6b6414c7532e090fc9b6193dfb8
Kim <kimberly.henry@outlook.com>
2022-12-04 14:28:45 -0500

    J9E-9 CH01 PE10

---
+++ Chapter_01/src/ch01_pe10_comments/Comments.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_pe10_comments;
+
+/*
+Project:       Java9E
+Module:      Chapter_01
+Package:     ch01_pe10_comments
+File:            Comments
+Created By: Kimberly Henry
+Created On: Sunday, 04-Dec-2022
+*/public class Comments
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Program comments are non-executing statements " +
+					"\nyou add to a file for the purpose of documentation.");
+			}
+	}
+	/*Program comments are non-executing statements you add to a file for the purpose of
+	documentation.*/
+
+/*Program comments are non-executing statements you add to a file for the purpose of
+documentation.*/
