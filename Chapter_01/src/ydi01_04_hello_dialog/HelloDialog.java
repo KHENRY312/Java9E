6b839eeb1923164ef5626fb9561c163d2ff95542
Kim <kimberly.henry@outlook.com>
2022-12-03 20:02:36 -0500

    J9E-3 Chap 01 You Do It

---
+++ Chapter_01/src/ydi01_04_hello_dialog/HelloDialog.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ydi01_04_hello_dialog;
+
+import javax.swing.*;
+
+/*
+Project:       Java9E
+Module:      Chapter_01
+Package:     ydi01_04_hello_dialog
+File:            HelloDialog
+Created By: Kimberly Henry
+Created On: Saturday, 03-Dec-2022
+
+*/public class HelloDialog
+	{
+	public static void main(String[] args)
+		{
+          JOptionPane.showMessageDialog(null, "Hello, world!",
+             "Hello Dialog",JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+
