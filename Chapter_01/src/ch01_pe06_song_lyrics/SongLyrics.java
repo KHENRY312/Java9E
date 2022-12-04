cfe87aaca3b0c9df501be2c119a0f44695398e14
Kim <kimberly.henry@outlook.com>
2022-12-03 20:33:25 -0500

    J9E-5 CH01 PE06

---
+++ Chapter_01/src/ch01_pe06_song_lyrics/SongLyrics.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_pe06_song_lyrics;
+
+/*
+Project:       Java9E
+Module:      Chapter_01
+Package:     ch01_pe06_song_lyrics
+File:            SongLyrics
+Created By: Kimberly Henry
+Created On: Saturday, 03-Dec-2022
+*/public class SongLyrics
+	{
+	public static void main(String[] args)
+		{
+			System.out.println("Each day through my window I watch as she passes by.");
+			System.out.println("I say to myself your such a lucky guy");
+			System.out.println("To have a girl like her is truly a dream come true.");
+			System.out.println("Out of all the guys in the world she she belongs to me.");
+			System.out.println("It was just my imagination running away with me.");
+			System.out.println("It was just my imagination once again, running away with me.");
+		}
+	}
