5835b8aaf32815f8e629a6b0ec82b20c637d3902
Kim <kimberly.henry@outlook.com>
2022-12-04 14:06:28 -0500

    J9E-7 CH01 PE08

---
+++ Chapter_01/src/ch01_pe08_table_and_chairs/TableAndChairs.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_pe08_table_and_chairs;
+
+import javax.swing.*;
+
+/*
+Project:       Java9E
+Module:      Chapter_01
+Package:     ch01_pe08_table_and_chairs
+File:            TableAndChairs
+Created By: Kimberly Henry
+Created On: Sunday, 04-Dec-2022
+*/public class TableAndChairs
+	{
+	public static void main(String[] args)
+       {
+           JOptionPane.showMessageDialog(null,
+              """
+                                     
+                 **                                                              **
+                 **                                                              **
+                 **                                                              **
+                 ************                             ************
+                 **            **   ** ************** **             **
+                 **            **   **                    **  **             **
+                 **            **   **                    **  **             **
+                 **            **   **                    **  **             **""",
+              "Table And Chairs",
+              JOptionPane.PLAIN_MESSAGE);
+       }
+	}
