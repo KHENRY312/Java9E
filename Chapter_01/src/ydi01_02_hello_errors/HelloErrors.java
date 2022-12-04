e445a708310e7b7752dca98b74b192f6b37ffbce
Kim <kimberly.henry@outlook.com>
2022-12-03 19:31:57 -0500

    J9E-51 YDI01_02-HelloErrors Code Style Added

---
+++ Chapter_01/src/ydi01_02_hello_errors/HelloErrors.java
@@ -0,0 +1,17 @@
+package ydi01_02_hello_errors;
+
+/*
+Project:         Java9E
+Module:       Chapter_01
+Package:      ydi01_02_hello_errors
+File:             HelloErrors
+Created By: Kimberly Henry
+Created On: Saturday, 03-Dec-2022
+*/public class HelloErrors
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Hello");
+				System.out.println("This is a test");
+			}
+	}
