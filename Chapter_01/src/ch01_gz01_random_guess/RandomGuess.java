2d36632a7495e4f4cda44fe993e3a04503b67d74
Kim <kimberly.henry@outlook.com>
2022-12-05 13:57:59 -0500

    J9E-64 CH01 GZ01 RandomGuess

---
+++ Chapter_01/src/ch01_gz01_random_guess/RandomGuess.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_gz01_random_guess;
+
+import javax.swing.*;
+
+/*
+Project:           Java9E
+Module:         Chapter_01
+Package:        ch01_gz01_random_guess
+File:               RandomGuess
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/
+public class RandomGuess
+	{
+		public static void main(String[] args)
+			{
+				JOptionPane.showMessageDialog(null, "Think of a number between 1 - 10",
+					"Guess A Number", JOptionPane.QUESTION_MESSAGE);
+				JOptionPane.showMessageDialog(null,"The number is "+ (1 + (int)(Math.random() * 10)),
+					"Correct Number", JOptionPane.INFORMATION_MESSAGE);
+			}
+	}
