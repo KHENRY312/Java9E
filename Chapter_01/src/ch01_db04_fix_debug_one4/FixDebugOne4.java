ccb14a3004dd29baec89e142f2588be05b4edbb9
Kim <kimberly.henry@outlook.com>
2022-12-05 06:57:45 -0500

    J9E-16 CH01 DB04

---
+++ Chapter_01/src/ch01_db04_fix_debug_one4/FixDebugOne4.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_db04_fix_debug_one4;
+
+/*
+Project:           Java9E
+Module:         Chapter_01
+Package:        ch01_db04_fix_debug_one4
+File:               FixDebugOne4
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/
+import javax.swing.JOptionPane;
+public class FixDebugOne4
+    {
+        public static void main(String[] args)
+        {
+            JOptionPane.showMessageDialog(null, "First GUI program");
+        }
+    }
