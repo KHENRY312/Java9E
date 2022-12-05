955fe199b584c073e4c5b698b9e72942d05f6bd5
Kim <kimberly.henry@outlook.com>
2022-12-05 06:41:09 -0500

    J9E-13 CH01 DB01

---
+++ Chapter_01/src/ch01_db01_fix_debug_one1/FixDebugOne1.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_db01_fix_debug_one1;
+
+/*
+Project:           Java9E
+Module:         Chapter_01
+Package:        ch01_db01_fix_debug_one1
+File:               FixedDebugOne1
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/
+public class FixDebugOne1
+	{
+   /* This program displays a greeting*/
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello");
+   }
+}
