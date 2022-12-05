d3142778302c76674515dea4824be82d25d60c94
Kim <kimberly.henry@outlook.com>
2022-12-05 06:53:15 -0500

    J9E-15 CH01 DB03

---
+++ Chapter_01/src/ch01_db03_fix_debug_one3/FixDebugOne3.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_db03_fix_debug_one3;
+
+/*
+Project:           Java9E
+Module:         Chapter_01
+Package:        ch01_db03_fix_debug_one3
+File:               FixDebugOne3
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/
+public class FixDebugOne3
+	{
+		public static void main(String[] args)
+			{
+				System.out.println("Over the river");
+				System.out.println("and through the woods");
+				System.out.println("to Grandmother's house we go");
+			}
+	}
