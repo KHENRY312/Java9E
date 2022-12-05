dfe2b847c8072e7d60d61278703925c45540255a
Kim <kimberly.henry@outlook.com>
2022-12-05 06:41:36 -0500

    J9E-3 Chap 01 You Do It

--- Chapter_01/src/ydi01_01_hello/Hello.java
+++ Chapter_01/src/ydi01_01_hello/Hello.java
@@ -1,9 +1,18 @@
-package ydi01_01_hello;
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ydi01_01_hello;/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
 /*
 Project:    Java9E
 Module:     Chapter_01
 Package:    ydi01_01_hello
-File:       Hello
+File:       ydi01_01_hello.Hello
 Created By: Kimberly Henry
 Created On: Saturday, 03-Dec-2022
 */
@@ -11,6 +20,6 @@ public class Hello
 	{
        public static void main(String[] args)
 			{
-				System.out.println("Hello, world!");
+				System.out.println("ydi01_01_hello.Hello, world!");
 			}
 	}
