01a88d3c3971102e36fb153ebf0d51a9998ef812
Kim <kimberly.henry@outlook.com>
2022-12-05 06:48:08 -0500

    J9E-14 CH01 DB02

---
+++ Chapter_01/src/ch01_db02_fix_debug_one2/FixDebugOne2.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_db02_fix_debug_one2;
+
+/*
+Project:           Java9E
+Module:         Chapter_01
+Package:        ch01_db02_fix_debug_one2
+File:               FixDebugOne2
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/
+public class FixDebugOne2
+	{
+		/* This program displays some output*/
+		public static void main(String[] args)
+			{
+				System.out.println("Java programming is fun.");
+				System.out.println("Getting a program to work");
+				System.out.println("can be a challenge,");
+				System.out.println("but when everything works correctly,");
+				System.out.println("it's very satisfying");
+			}
+	}
