c9908934b144ec5206a9573f148b87088518a751
Kim <kimberly.henry@outlook.com>
2022-12-05 05:42:34 -0500

    J9E-11 CH01 PE12

---
+++ Chapter_01/src/ch01_pe12_burma_shave/BurmaShave.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch01_pe12_burma_shave;
+
+import javax.swing.*;
+
+/*
+Project:          Java9E
+Module:        Chapter_01
+Package:       ch01_pe12_burma_shave
+File:              BurmaShave
+Created By:   Kimberly Henry
+Created On:  Monday, 05-Dec-2022
+*/public class BurmaShave
+	{
+		public static void main(String[] args)
+			{
+             JOptionPane.showMessageDialog( null, """
+                   Approached a crossing.
+                   without looking,
+                   Who will eat,\s
+                   his widow's cooking.""",
+                "BURMA SHAVE", JOptionPane.PLAIN_MESSAGE);
+			}
+	}
