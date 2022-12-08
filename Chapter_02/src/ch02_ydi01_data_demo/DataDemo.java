51975d98127811ddb33c284640585d1ac3a3ab42
Kim <kimberly.henry@outlook.com>
2022-12-08 07:54:38 -0500

    J9E-41 CH02 YDI01 DataDemo

---
+++ Chapter_02/src/ch02_ydi01_data_demo/DataDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch02_ydi01_data_demo;
+
+/*
+Project:           Java9E
+Module:         Chapter_02
+Package:        ch02_ydi01_data_demo
+File:               DataDemo
+Created By:   Kimberly Henry
+Created On:  Thursday, 08-Dec-2022
+*/
+public class DataDemo
+	{
+		public static void main(String[] args)
+			{
+				int aWholeNumber = 315;
+				System.out.print("The number is ");
+				System.out.println(aWholeNumber);
+			}
+	}
