da4a8eb269e36e1cdda41fc557ced8532b6e6574
Kim <kimberly.henry@outlook.com>
2022-12-08 19:57:05 -0500

    J9E-10 CH01 PE11

--- Chapter_01/src/ch01_pe11_comments_dialog/CommentsDialog.java
+++ Chapter_01/src/ch01_pe11_comments_dialog/CommentsDialog.java
@@ -7,6 +7,9 @@ package ch01_pe11_comments_dialog;
 
 import javax.swing.*;
 
+/**
+ * The type Comments dialog.
+ */
 /*
 Project:       Java9E
 Module:      Chapter_01
@@ -16,6 +19,11 @@ Created By: Kimberly Henry
 Created On: Monday, 05-Dec-2022
 */public class CommentsDialog
 	{
+		/**
+		 * The entry point of application.
+		 *
+		 * @param args the input arguments
+		 */
 		public static void main(String[] args)
 			{
 				JOptionPane.showMessageDialog(null, """
