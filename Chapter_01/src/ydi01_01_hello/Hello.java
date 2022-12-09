21eab75b4e387c310d072f080d920bbef5e4c45e
Kim <kimberly.henry@outlook.com>
2022-12-08 19:58:12 -0500

    J9E-50 YDI01_01-Hello

--- Chapter_01/src/ydi01_01_hello/Hello.java
+++ Chapter_01/src/ydi01_01_hello/Hello.java
@@ -8,6 +8,9 @@ package ydi01_01_hello;/*
  * and not to be used in lieu of performing your own assignments.
  */
 
+/**
+ * The type Hello.
+ */
 /*
 Project:    Java9E
 Module:     Chapter_01
@@ -18,7 +21,12 @@ Created On: Saturday, 03-Dec-2022
 */
 public class Hello
 	{
-       public static void main(String[] args)
+		/**
+		 * The entry point of application.
+		 *
+		 * @param args the input arguments
+		 */
+		public static void main(String[] args)
 			{
 				System.out.println("ydi01_01_hello.Hello, world!");
 			}
