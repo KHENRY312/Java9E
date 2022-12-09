32883775037f074874a12965ee444403527a24ad
Kim <kimberly.henry@outlook.com>
2022-12-08 19:56:48 -0500

    J9E-8 CH01 PE09

--- Chapter_01/src/ch01_pe09_triangle/Triangle.java
+++ Chapter_01/src/ch01_pe09_triangle/Triangle.java
@@ -7,6 +7,9 @@ package ch01_pe09_triangle;
 
 import javax.swing.*;
 
+/**
+ * The type Triangle.
+ */
 /*
 Project:       Java9E
 Module:      Chapter_01
@@ -16,6 +19,11 @@ Created By: Kimberly Henry
 Created On: Sunday, 04-Dec-2022
 */public class Triangle
 	{
+		/**
+		 * The entry point of application.
+		 *
+		 * @param args the input arguments
+		 */
 		public static void main(String[] args)
 			{
 				JOptionPane.showMessageDialog (null, """
