e98fac980cd0ea111d08423b5c5b601671136e01
Kim <kimberly.henry@outlook.com>
2022-12-08 20:01:47 -0500

    J9E-43 CH02 YDI03 CharDemo

---
+++ Chapter_02/src/ch02_ydi03_char_demo/CharDemo.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch02_ydi03_char_demo;
+
+/*
+Project:           Java9E
+Module:         Chapter_02
+Package:        ch02_ydi03_char_demo
+File:               CharDemo
+Created By:   Kimberly Henry
+Created On:  Thursday, 08-Dec-2022
+*/
+public class CharDemo
+	{
+		public static void main(String[] args)
+			{
+             char initial = 'A';
+             System.out.println(initial);
+             System.out.print("\t\"abc\\def\bghi\n\njkl");
+			}
+	}
