dd24495e910871dee65f8448df0b302585f4fecf
Kim <kimberly.henry@outlook.com>
2022-12-08 19:58:34 -0500

    J9E-52 YDI01_03-Hello2

--- Chapter_01/src/ydi01_03_hello2/Hello2.java
+++ Chapter_01/src/ydi01_03_hello2/Hello2.java
@@ -5,6 +5,9 @@
 
 package ydi01_03_hello2;
 
+/**
+ * The type Hello 2.
+ */
 /*
 Project:       Java9E
 Module:      Chapter_01
@@ -14,7 +17,12 @@ Created By: Kimberly Henry
 Created On: Saturday, 03-Dec-2022
 */public class Hello2
 	{
-	public static void main(String[] args)
+		/**
+		 * The entry point of application.
+		 *
+		 * @param args the input arguments
+		 */
+		public static void main(String[] args)
 		{
 		  System.out.println("ydi01_01_hello.Hello, world!");
         
