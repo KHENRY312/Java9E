78c96dd5f05415b012c7307f5cf96133f6bae6e5
Kim <kimberly.henry@outlook.com>
2022-12-09 23:44:21 -0500

    J9E-47 CH02 YDI08 ArithmeticDemo3

---
+++ Chapter_02/src/ch02_ydi08_arithmetic_demo3/ArithmeticDemo3.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch02_ydi08_arithmetic_demo3;
+
+import java.util.Scanner;
+
+/*
+Project:           Java9E
+Module:         Chapter_02
+Package:        ch02_ydi08_arithmetic_demo3
+File:               ArithmeticDemo3
+Created By:   Kimberly Henry
+Created On:  Friday, 09-Dec-2022
+*/
+public class ArithmeticDemo3
+	{
+		public static void main(String[] args)
+			{
+				int firstNumber;
+				int secondNumber;
+				int sum;
+				int difference;
+				double average;
+				Scanner keyboard = new Scanner(System.in);
+				System.out.print("Please enter an integer >> ");
+				firstNumber = keyboard.nextInt();
+				System.out.print("Please enter another integer >> ");
+				secondNumber = keyboard.nextInt();
+				sum = firstNumber + secondNumber;
+				difference = firstNumber - secondNumber;
+				average = (double) sum / 2;
+				System.out.println(firstNumber + " + " + secondNumber + " is " + sum);
+				System.out.println(firstNumber + " - " + secondNumber + " is " + difference);
+				System.out.println("The average of " + firstNumber + " and " + secondNumber + " is " + average);
+			}
+	}
