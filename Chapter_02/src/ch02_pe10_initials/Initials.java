38eaaa7f9e442176fdda199bd7b85b4ff1a6a3fd
Kim <kimberly.henry@outlook.com>
2022-12-13 00:03:27 -0500

    J9E-27 CH02 PE10

---
+++ Chapter_02/src/ch02_pe10_initials/Initials.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch02_pe10_initials;
+
+/*
+Project:           Java9E
+Module:         Chapter_02
+Package:        ch02_pe10_initials
+File:               Initials
+Created By:   Kimberly Henry
+Created On:  Monday, 12-Dec-2022
+*/
+public class Initials
+	{
+		public static void main(String[] args)
+			{
+				char first =  'K';
+				char last = 'H';
+				char period = '.';
+				String space = " ";
+				System.out.print("My initials are " + first + period + space + last + period);
+			}
+	}
