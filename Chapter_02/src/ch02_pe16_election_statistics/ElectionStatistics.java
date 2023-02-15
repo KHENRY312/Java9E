a63a285b736545e8a82424f3d5c92a21a9335c90
Kimberly <kimberly.henry@outlook.com>
2023-02-15 14:09:54 -0500

    J9E-33 CH02 PE16

---
+++ Chapter_02/src/ch02_pe16_election_statistics/ElectionStatistics.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2023. This code shall not be used, manipulated, decompiled or reproduced for other uses than educational;
+ * and not to be used in lieu of performing your own assignments.
+ */
+
+package ch02_pe16_election_statistics;
+/*
+    Project:    Java9E
+    Module:     Chapter_02
+    Package:    ch02_pe16_election_statistics
+    Class:      ElectionStatistics
+    Creator:    Kimberly Henry
+    Created:    2/14/2023 1:24 AM
+*/
+public class ElectionStatistics
+	{
+	public static void main(String[] args)
+		{
+			//todo
+		}
+	}
